09668461e274f35674cbc2ef0c99cb80e817629d
Jean-Francois St-Germain <jfstgermain@gmail.com>
2017-03-16 15:19:24 -0400

    added length to options

--- typeorm-model-generator.js
+++ typeorm-model-generator.js
@@ -2,22 +2,25 @@ const _ = require('lodash');
 const csv = require('csv');
 const fs = require('fs');
 
-const colDefTemplate = _.template("@Column('<%= types.ormType %>', {name: '<%= dbName %>'})\npublic <%= _.camelCase(dbName) %>: <%= types.jsType %>;");
+const colDefTemplate = _.template("@Column('<%= types.ormType %>', {name: '<%= dbName %>'<% if(types.length) {%>, length: <%= types.length %><% } %>})\npublic <%= _.camelCase(dbName) %>: <%= types.jsType %>;");
 
 function convertToJsTypes (type) {
   let types = null;
+  let matchRes;
 
-  if (type.match(/integer|number/i)) {
+  if (matchRes = type.match(/integer|number(\((\d+)\))?/i)) {
     types = {
       ormType: 'number',
       jsType: 'number',
+      length: matchRes[2],
     };
-  } else if (type.match(/string.*|varchar.*/i)) {
+  } else if (matchRes = type.match(/string.*|varchar2?(\((\d+)\))?/i)) {
     types = {
       ormType: 'string',
       jsType: 'string',
+      length: matchRes[2],
     };
-  } else if (type.match(/date.*/i)) {
+  } else if (matchRes = type.match(/date.*/i)) {
     types = {
       ormType: 'date',
       jsType: 'Date',
